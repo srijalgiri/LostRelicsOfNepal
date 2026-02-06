aa64c25599a5ba03c8b0894c5a73923001641e57
srijalgiri <np03cs4a230433@heraldcollege.edu.np>
2026-02-06 16:11:29 +0545

    Add files via upload

---
+++ AnimationString.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public static class AnimationStrings
+{
+    public static string isMoving = "isMoving";
+    public static string isRunning = "isRunning";
+    public static string isGrounded = "isGrounded";
+    public static string yVelocity = "yVelocity"; 
+    public static string jumpTrigger = "jump"; 
+    public static string isOnWall = "isOnWall";
+    public static string isOnCeiling = "isOnCeiling";
+    public static string attackTrigger = "attack";
+    public static string canMove = "canMove";
+    public static string hasTarget = "hasTarget";
+    public static string isAlive = "isAlive";
+    public static string isHit = "isHit";
+    public static string hitTrigger = "hit";
+    public static string lockVelocity = "lockVelocity";
+
+    public static string deathTrigger = "death"; 
+}
